3c1e861f51f79ea1b9e9e23e06575c167fc75fa1
Farid <pesarkhobeee@gmail.com>
2020-07-29 07:29:09 +0200

    Add the input arguments handler

---
+++ tcg.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+import sys
+import os
+
+
+def yaml_generator(args):
+    """
+    The code generator accept the path to a yaml config file,
+    Then it will generate the yaml file with help of a Jinja2 template
+    And at last it wil use the output directory to save the generated .tf file
+    """
+
+    if not os.path.exists(args.Source):
+        raise Exception('The source file doesn\'t exist: {}'.format(args.Source))
+
+    if not os.path.exists(args.Destination):
+        raise Exception('The destination folder doesn\'t exist: {}'.format(args.Destination))
+
+    return "It is working!"
+
+
+def main(args):
+    try:
+        print(yaml_generator(args))
+    except Exception as e:
+        logging.critical("Fatal error hapend: {}".format(e))
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Source Destination")
+    parser.add_argument(
+        'Source', type=str,
+        help='The configuration file location'
+    )
+    parser.add_argument(
+        'Destination', type=str,
+        help='The output directory for the generated .tf file'
+    )
+    parser.add_argument(
+        '--log-level', type=str, default="WARNING",
+        help='Set the logging level. Defaults to WARNING.'
+    )
+    parsed_args = parser.parse_args()
+    logging.getLogger()
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)s %(message)s',
+        level=parsed_args.log_level,
+        datefmt='%Y-%m-%dT%H:%M:%S%z')
+    logging.info(
+        "Starting with given arguments: {}".format(parsed_args)
+    )
+    main(parsed_args)
